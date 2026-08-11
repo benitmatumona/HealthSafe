24ab8eb1c1e9dd6eb4ff8acdec760e8e07da42ab
benitmatumona <bematjhb025@student.wethinkcode.co.za>
2026-08-11 09:09:51 +0200

    ResourceNotFoundException

---
+++ src/main/java/com/benitmatumona/healthsafe/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.benitmatumona.healthsafe.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+
+
+    public ResourceNotFoundException(String message){
+
+        super(message);
+
+    }
+
+}
